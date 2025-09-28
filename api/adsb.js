8cb5b470dceb2c5f4c039b3a1f0b3bcb33610da5
pechilinw4f <159911807+pechilinw4f@users.noreply.github.com>
2025-09-29 00:40:01 +0200

    maj

--- api/adsb.js
+++ api/adsb.js
@@ -1,17 +1,12 @@
-import fetch from 'node-fetch'; // obligatoire si Node < 18
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   const { lat, lon, radius } = req.query;
-
   if (!lat || !lon || !radius) {
     return res.status(400).json({ error: "Missing query parameters lat, lon, radius" });
   }
-
   try {
     const response = await fetch(`https://api.adsb.lol/v2/point/${lat}/${lon}/${radius}`);
-    if (!response.ok) {
-      return res.status(response.status).json({ error: "ADSB API error" });
-    }
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json(data);
